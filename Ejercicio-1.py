a49bc3d6b2064a18940c4cb0a8f77493d3ecd5a5
ciroribba <ciro.ribba@gmail.com>
2024-08-30 20:07:51 -0300

    add module 1.1

---
+++ Ejercicio-1.py
@@ -0,0 +1,32 @@
+import course_python_3.Constantes as Constantes
+print('Hola Mundo')
+nombre = 'Ciro'
+print(nombre)
+# para concatenar es necesario colocar coma o + (si son string) entre las variables 
+# numeral es para comentarios
+"""
+Este es un comentario de varias lineas
+"""
+print('Hola mi nombre es:', nombre)
+print('Hola mi nombre es: ' + nombre)
+print('Variables')
+variablestring = 'Luis'
+print(variablestring)
+variableint = 10
+print(variableint)
+variablebool = True
+print(variablebool)
+variablefloat = 10.5
+print(variablefloat)
+print('Constantes - se recomienda que las constantes se escriban en mayusculas')
+CONSTANTE = 'Soy una constante'
+print(CONSTANTE)
+print(Constantes.PI)
+variable1 = variable2 = variable3 = 'Aureliano'
+print(variable1)
+print(variable2)
+print(variable3)
+variable4, variable5, variable6 = 'Aureliano2', 'Ciro', 'Luis'
+print(variable4)
+print(variable5)
+print(variable6)
